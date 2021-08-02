552a81b4c4606243241f488ad19104c1ee57f9c3
Ananda Hasibuan <anandahasibuan13@gmail.com>
2021-08-02 16:23:22 +0700

    customize bootstrap with scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AllComponent from './Component/AllComponent';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './css/main.css'
 
 ReactDOM.render(
   <React.Fragment>
